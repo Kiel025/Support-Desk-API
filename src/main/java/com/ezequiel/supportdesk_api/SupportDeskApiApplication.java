33f5cd1cc58c0d981f84be7a1dd4e33e56b9ba2f
kiel025 <kiel02505@gmail.com>
2025-12-22 15:12:08 -0300

    feat: project files

---
+++ src/main/java/com/ezequiel/supportdesk_api/SupportDeskApiApplication.java
@@ -0,0 +1,13 @@
+package com.ezequiel.supportdesk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportDeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportDeskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezequiel/supportdesk_api/SupportDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezequiel.supportdesk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
