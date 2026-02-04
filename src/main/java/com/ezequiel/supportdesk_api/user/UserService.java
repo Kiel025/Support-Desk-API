0f3b7b21ca0db177e88816193a1212e3a75b816a
kiel025 <kiel02505@gmail.com>
2026-02-04 18:02:58 -0300

    feat: add getAllUsers and saveUser methods

--- src/main/java/com/ezequiel/supportdesk_api/user/UserService.java
+++ src/main/java/com/ezequiel/supportdesk_api/user/UserService.java
@@ -1,10 +1,12 @@
 package com.ezequiel.supportdesk_api.user;
 
 import com.ezequiel.supportdesk_api.exceptions.UserNotFoundException;
+import com.ezequiel.supportdesk_api.user.dto.UserRequestDTO;
 import com.ezequiel.supportdesk_api.user.dto.UserResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -21,6 +23,21 @@ public class UserService {
     public UserResponseDTO getByEmail(String email) {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Error: User with email " + email + " not found."));
         return convertUser2UserResponseDTO(user);
+    }
+
+    public List<UserResponseDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(this::convertUser2UserResponseDTO).toList();
+    }
+
+    public UserResponseDTO saveUser(UserRequestDTO userRequestDTO) {
+        User user = new User();
+        user.setName(userRequestDTO.name());
+        user.setEmail(userRequestDTO.email());
+        user.setRole(userRequestDTO.role());
+        user.setPasswordHash(userRequestDTO.password());
+
+        return convertUser2UserResponseDTO(userRepository.save(user));
     }
 
     private UserResponseDTO convertUser2UserResponseDTO(User user) {
